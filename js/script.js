0796bf9317672bf590fd1e0baddfba14d55b37f7
BRIAN HICKS <bhicks@risd.edu>
2015-05-13 21:00:14 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,41 +1,30 @@
 $(document).ready(function(){
 
 	console.log("Hi there buddy");
-
-	//jGravity
-	$('body').jGravity()
 	
 	//click events on sections
 	$('#bikesText, #wheelsText, #cleatsText, #helmetsText, #extrasText').click( function() {
 		$(this).addClass('animated zoomOut');
 	});
-	$('#bikesText').click( function() {
-		$('.bike').addClass('animated zoomIn visible');
-	});
-	$('#wheelsText').click( function() {
-		$('.wheel').addClass('animated zoomIn visible');
-	});
-	$('#cleatsText').click( function() {
-		$('.cleat').addClass('animated zoomIn visible');
-	});
-	$('#helmetsText').click( function() {
-		$('.helmet').addClass('animated zoomIn visible');
-	});
-	$('#extrasText').click( function() {
-		$('.extra').addClass('animated zoomIn visible');
+	$('.Text').click( function() {
+		$('body').jGravity();
 	});
 
-	//description in and out
-	//$('#names').mouseover( function() {
-	//	$('#description').addClass('visible animated lightSpeedIn');
-	//});
-	//$('#names').mouseout( function() {
-	//	$('#description').addClass('hidden animated lightSpeedOut');
-	//});
+	$('.Text').live('click', function() {
+          $('body').jGravity({
+               target: 'everything',
+               ignoreClass: 'what',
+               drag: true
+          });
+     });
 
 	//draggable
 	$(function() {
-    	$( "img" ).draggable();
+    	$( "div" ).draggable();
   	});
 	console.log("draggable working");
+
+	$('#what').click( function() {
+		$('#description').addClass('visible animated lightSpeedIn');
+	});
 });
