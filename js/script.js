8f8191c06ec6d097d391854859f811227372708e
BRIAN HICKS <bhicks@risd.edu>
2015-05-15 18:28:07 -0400

    simple

--- js/script.js
+++ js/script.js
@@ -3,26 +3,20 @@ $(document).ready(function(){
 	console.log("Hi there buddy");
 	
 	//click events on sections
-	$('#bikesText, #wheelsText, #cleatsText, #helmetsText, #extrasText').click( function() {
-		$(this).addClass('animated zoomOut');
-	});
-	$('.Text').click( function() {
-		$('body').jGravity();
-	});
+	// $('#bikesText, #wheelsText, #cleatsText, #helmetsText, #extrasText').click( function() {
+	// 	$(this).addClass('animated zoomOut');
+	// });
+	// $('.text').click( function() {
+	// 	$('body').jGravity();
+	// });
 
-	$('.Text').live('click', function() {
+	$('.text').click( function() {
           $('body').jGravity({
                target: 'everything',
                ignoreClass: 'what',
                drag: true
           });
      });
-
-	//draggable
-	$(function() {
-    	$( "div" ).draggable();
-  	});
-	console.log("draggable working");
 
 	$('#what').click( function() {
 		$('#description').addClass('visible animated lightSpeedIn');
